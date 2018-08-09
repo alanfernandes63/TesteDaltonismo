a7b0a57658c49df3985887db76f307f459b5cd89
Aluno <alan.fernandes63@gmail.com>
2018-08-09 11:14:31 -0300

    versaao 1.1

--- app/src/main/java/br/ufrn/eaj/tads/testedaltonismo/MainActivity.java
+++ app/src/main/java/br/ufrn/eaj/tads/testedaltonismo/MainActivity.java
@@ -1,13 +1,68 @@
 package br.ufrn.eaj.tads.testedaltonismo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    int botao;
+    String valor;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Bundle param = new Bundle();
+        int botao = param.getInt("botao");
+        valor = param.getString("valor");
+        resposta();
+    }
+
+    public void resposta(){
+
+        Toast.makeText(this, "botao " + botao, Toast.LENGTH_SHORT).show();
+        if(botao != 0){
+            TextView r1 = findViewById(R.id.textView5);
+            TextView r2 = findViewById(R.id.textView6);
+            TextView r3 = findViewById(R.id.textView7);
+            switch (botao){
+                case 1:
+                  r1.setText(valor);
+            }
+        }
+    }
+
+
+    public void view2(View v){
+        Intent i = new Intent(this,TesteActivity.class);
+        Bundle param = new Bundle();
+
+
+        if (R.id.button == v.getId()){
+            param.putInt("botao",1);
+        }
+        else if(R.id.button2 == v.getId()){
+            param.putInt("botao",2);
+        }
+        else if(R.id.button3 == v.getId()){
+            param.putInt("botao",3);
+        }
+        i.putExtras(param);
+
+        startActivity(i);
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(resultCode == RESULT_CANCELED)
+            System.out.print("ada");
+
     }
 }
--- app/src/main/java/br/ufrn/eaj/tads/testedaltonismo/TesteActivity.java
+++ app/src/main/java/br/ufrn/eaj/tads/testedaltonismo/TesteActivity.java
@@ -1,13 +1,53 @@
 package br.ufrn.eaj.tads.testedaltonismo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class TesteActivity extends AppCompatActivity {
-
+    int botao;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_teste);
+
+        ImageView img = findViewById(R.id.imageView);
+
+        Bundle param = getIntent().getExtras();
+        int indice = param.getInt("botao");
+        botao = indice;
+        if(indice == 1){
+            img.setImageDrawable(getResources().getDrawable(R.drawable.teste1));
+        }
+        else if(indice == 2){
+            img.setImageDrawable(getResources().getDrawable(R.drawable.teste2));
+        }
+        else if(indice == 3){
+            img.setImageDrawable(getResources().getDrawable(R.drawable.teste3));
+        }
+    }
+
+    public void ok(View v){
+        EditText texto = findViewById(R.id.editText);
+        Intent i = new Intent(this,MainActivity.class);
+        Bundle param = new Bundle();
+        Toast.makeText(this, "" + botao, Toast.LENGTH_SHORT).show();
+        param.putInt("botao",botao);
+        //param.putString("valor",texto.getText().toString());
+        if (texto.getText().toString().equals("")) {
+            Toast.makeText(this, "Insira um valor!", Toast.LENGTH_SHORT).show();
+        }
+        i.putExtras(param);
+        startActivity(i);
+    }
+
+    public  void cancelar(View view){
+        setResult(RESULT_CANCELED);
+        finish();
     }
 }
